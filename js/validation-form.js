a943d8831bd2933cc0ce5b962bdbf375f5d1a316
yodavan <84133554+yodavan@users.noreply.github.com>
2022-03-12 22:18:58 +0000

    Произвел рефакторинг кода

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,48 +1,33 @@
-const INPUT_PRICE = {
-  inputItem: document.querySelector( '#price' ),
-  min: 0,
-  max: 100000,
-  value: 'руб.'
-};
-
-const INPUT_TITLE = {
-  inputItem: document.querySelector( '#title' ),
-  min: 30,
-  max: 100,
-  value: 'символов'
-};
+const INPUT_PRICE = document.querySelector( '#price' );
+const INPUT_TITLE = document.querySelector( '#title' );
 
 const getValueInput = ( element ) => {
-  if ( element.inputItem.value < element.min ) {
-    element.inputItem.setCustomValidity( `Увеличьте минимум на ${ element.min - element.inputItem.value } ${ element.value }` );
-  } else if ( element.inputItem.value > element.max ) {
-    element.inputItem.setCustomValidity( `Уменьшите минимум на ${ element.inputItem.value - element.max } ${ element.value }` );
+  if ( element.value < +element.min ) {
+    element.setCustomValidity( `Увеличьте минимум на ${ +element.min - element.value } руб.` );
+  } else if ( element.value > +element.max ) {
+    element.setCustomValidity( `Уменьшите минимум на ${ element.value - +element.max } руб.` );
   } else {
-    element.inputItem.setCustomValidity( '' );
+    element.setCustomValidity( '' );
   }
 
-  element.inputItem.reportValidity();
+  element.reportValidity();
 };
 
 const getLengthString = ( element ) => {
-  if ( element.inputItem.value.length < element.min ) {
-    element.inputItem.setCustomValidity( `Увеличьте минимум на ${ element.min - element.inputItem.value.length } ${ element.value }` );
-  } else if ( element.inputItem.value.length > element.max ) {
-    element.inputItem.setCustomValidity( `Уменьшите минимум на ${ element.inputItem.value.length - element.max } ${ element.value }` );
+  if ( element.value.length < +element.minLength ) {
+    element.setCustomValidity( `Увеличьте минимум на ${ +element.minLength - element.value.length } символов` );
+  } else if ( element.value.length > +element.maxLength ) {
+    element.setCustomValidity( `Уменьшите минимум на ${ element.value.length - +element.maxLength } символов` );
   } else {
-    element.inputItem.setCustomValidity( '' );
+    element.setCustomValidity( '' );
   }
 
-  element.inputItem.reportValidity();
+  element.reportValidity();
 };
 
-INPUT_PRICE.inputItem.addEventListener('input', () => {
-  getValueInput( INPUT_PRICE );
-});
+INPUT_PRICE.addEventListener('input', () => getValueInput( INPUT_PRICE ));
 
-INPUT_TITLE.inputItem.addEventListener('input', () => {
-  getLengthString( INPUT_TITLE );
-});
+INPUT_TITLE.addEventListener('input', () => getLengthString( INPUT_TITLE ));
 
 // Отключить кнопку в зависимости от выбора
 const SELECTION_VALUE = {
@@ -71,9 +56,7 @@ OPTIONS_ROOM.forEach(( itemRoom ) => {
     const capscity = document.querySelector( '#capacity' );
     const options = capscity.querySelectorAll( 'option' );
 
-    options.forEach(( item ) => {
-      getDisabledElements( SELECTION_VALUE[itemRoom.value], item );
-    });
+    options.forEach(( item ) => getDisabledElements( SELECTION_VALUE[itemRoom.value], item ));
   }
 });
 
@@ -81,7 +64,5 @@ ROOM_NUMBER.addEventListener('change', (evt) => {
   const capscity = document.querySelector( '#capacity' );
   const options = capscity.querySelectorAll( 'option' );
 
-  options.forEach(( item ) => {
-    getDisabledElements( SELECTION_VALUE[evt.target.value], item );
-  });
+  options.forEach(( item ) => getDisabledElements( SELECTION_VALUE[evt.target.value], item ));
 });
